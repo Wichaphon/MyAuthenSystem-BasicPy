2e0a4a1c45fd12c657e27d7b599eabd87f9d4f7c
Wichaphon <horokindov3310@gmail.com>
2025-05-03 23:22:26 +0700

    auth blueprint and serve static files

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,10 @@
-from flask import Flask , request , jsonify
+from flask import Flask
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 
-@app.route('/login' , methods=['POST'])
+# Register Blueprints
+app.register_blueprint(auth_bp)
 
-def login():
-    data = request.json
-    user = data.get("user")
-    password = data.get("password")
-
-    if user == "admin" and password == "1234":
-        return jsonify({"status": "success"})
-    else:
-        return jsonify({"status": "fail"}), 401
-    
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
