f7c450f8ba475ffeb12ed92e041222c8b3cbfe45
Wichaphon <horokindov3310@gmail.com>
2025-05-05 17:38:59 +0700

    Init fullstack version in wichaphonfull branch

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from routes.auth import auth_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+#อนุญาตให้ frontend ต่างโดเมน (เช่น React) เข้าถึงAPIนี้ได้
+CORS(app) #browser บังคับตรวจ Cross-Origin Resource Sharing ไม่ใส่จะ fetch ไม่ได้ (กันลืม)
 
 # Register Blueprints
 app.register_blueprint(auth_bp)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginForm from './Components/LoginForm'
+
+export default function App() {
+
+  return (
+    <>
+      <LoginForm/>
+    </>
+      
+  )
+}
+
+
