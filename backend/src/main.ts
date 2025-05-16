0b5e2b2f9eb60f7c36f0501003eda06ca931857c
Wichaphon <horokindov3310@gmail.com>
2025-05-16 18:20:22 +0700

    Remove old Python backend

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-// import './types/express'; // 👈 ต้องใส่บนสุดเพื่อขยาย Request.user
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000); // ตรงกับ docker-compose ที่ expose 5000
-}
-bootstrap();
--- backend/src/types/jwt-payload.ts
+++
@@ -1,6 +0,0 @@
-// src/types/jwt-payload.ts
-export interface JwtPayload {
-  myname: string;
-  myposition: string;
-  picture: string;
-}
